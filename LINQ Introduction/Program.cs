480abe1967fdd7eaeee6f39ca7d2ca28260df98c
peerhoffmanncode <peerhoffmann81@gmail.com>
2023-08-13 00:19:51 +0200

    LinQ intro

---
+++ LINQ Introduction/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var numbers = new List<int> { 1, 2, 3, 4, 5 };
+var numbersWith10 = numbers.Append(10);
+numbersWith10 = numbersWith10.Append(11);
+
+var oddNumber = numbersWith10
+    .Where(x => x % 2 == 1)
+    .OrderBy(x => -x);
+
+foreach (var number in oddNumber)
+{
+    Console.WriteLine(number);
+}
+
+
+
+Console.ReadKey();
+
+
+
